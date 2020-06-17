cef942228eb63e518d4e94dde09f48655ec60d37
OmerOhana <oohana13@gmail.com>
2020-06-17 16:48:13 +0300

    Updates

---
+++ precache-manifest.07ed99db5ef858b46877d6b9ba2f64c4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2bc265777b3ac86597baf9f110112500",
+    "url": "/MonstersRolodex/index.html"
+  },
+  {
+    "revision": "fc27cff13c792b3d6e11",
+    "url": "/MonstersRolodex/static/css/main.72ccf569.chunk.css"
+  },
+  {
+    "revision": "04cf369bcbe212695587",
+    "url": "/MonstersRolodex/static/js/2.cd22e9b4.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/MonstersRolodex/static/js/2.cd22e9b4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fc27cff13c792b3d6e11",
+    "url": "/MonstersRolodex/static/js/main.e8196c48.chunk.js"
+  },
+  {
+    "revision": "5db7818027d4f55ae832",
+    "url": "/MonstersRolodex/static/js/runtime-main.21236790.js"
+  }
+]);
